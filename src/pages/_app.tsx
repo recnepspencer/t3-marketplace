4b16cc6e50d324d40c122672385c8a3ff2d448bc
recnepspencer <goldenspencerh@gmail.com>
2023-05-24 16:18:37 -0600

    added navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { withTRPC } from "@trpc/next";
 import type { AppProps } from "next/app";
 import { api } from "~/utils/api";
+import "~/styles/globals.css";
+import { NavBar } from "~/components/Navbar";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
+      <NavBar/>
       <Component {...pageProps} />
     </ClerkProvider>
   );
